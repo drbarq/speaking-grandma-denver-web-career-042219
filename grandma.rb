b8fbbccb4c2b89eab5024746cb332f26d62e4b5c
Joe Tustin <j.tustin@gmail.com>
2019-03-11 23:06:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(statement)
-  if statement == statement.uppercase
+  if statement == statement.upcase
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
