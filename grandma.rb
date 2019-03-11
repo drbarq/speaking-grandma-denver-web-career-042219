bb01b8387be36b5216622cf957491d80be95f7be
Joe Tustin <j.tustin@gmail.com>
2019-03-11 23:09:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,7 @@
 def speak_to_grandma(statement)
   if statement == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  end
-  if statement == statement.upcase
+  elsif statement == statement.upcase
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
